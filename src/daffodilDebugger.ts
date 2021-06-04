0e64473fc8481231927c46a26c43e9218c2d61cb
Shane Dell <32347414+Shanedell@users.noreply.github.com>
2021-06-04 13:58:56 -0400

    update extension to download executable files to User_Home_Folder/daffodil-debug (#21)
    update the downloading of the debugger files into the home directory to
    work on windows
    
    delete some bad white space and comments
    
    update location of the downloaded files to go to tmp folder using os.tmpdir
    
    update location of downloaded files
    
    fix data directory name
    `
    
    get rid of information message

--- src/daffodilDebugger.ts
+++ src/daffodilDebugger.ts
@@ -5,6 +5,9 @@ import * as os from 'os';
 import * as child_process from 'child_process';
 import { HttpClient } from 'typed-rest-client/HttpClient';
 import { RestClient } from 'typed-rest-client/RestClient';
+import XDGAppPaths from 'xdg-app-paths';
+const xdgAppPaths = XDGAppPaths({"name": "dapodil"})
+
 
 // Class for getting release data
 export class Release {
@@ -28,7 +31,7 @@ export async function getDebugVersion(config: vscode.DebugConfiguration) {
     if (!config.dapodilVersion) {        
         const client = new RestClient("client")
         let request = await client.get<Release[]>('https://api.github.com/repos/jw3/example-daffodil-debug/tags')
-        
+
         if (request.statusCode !== 200 || !request.result) {
             const err: Error = new Error(`Check request url, and tags of the repo. Follow this template if not already https://api.github.com/repos/{owner}/{rep_name}/tags`);
             err["httpStatusCode"] = request.statusCode;
@@ -48,7 +51,6 @@ export async function getDebugVersion(config: vscode.DebugConfiguration) {
     return config.dapodilVersion?.includes("v") ? config.dapodilVersion : `v${config.dapodilVersion}`;
 }
 
-
 // Function for getting the da-podil debugger
 export async function getDebugger(config: vscode.DebugConfiguration) {
     // If useExistingServer var set to false make sure version of debugger entered is downloaded then ran
@@ -58,10 +60,11 @@ export async function getDebugger(config: vscode.DebugConfiguration) {
         const delay = ms => new Promise(res => setTimeout(res, ms));
 
         if(vscode.workspace.workspaceFolders !== undefined) {
-            let rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+            let rootPath = xdgAppPaths.data()
 
-            if (os.platform() === 'win32') {
-                rootPath = rootPath.substring(1); // For windows the / at the start causes issues
+            // If directory for storing debugger does exist create it
+            if (!fs.existsSync(rootPath)) {
+                fs.mkdirSync(rootPath)
             }
 
             // Code for downloading and setting up da-podil files
@@ -90,7 +93,7 @@ export async function getDebugger(config: vscode.DebugConfiguration) {
                 });
 
                 // Unzip file and remove zip file
-                await new Promise ((res, rej) => { 
+                await new Promise ((res, rej) => {
                     let stream = fs.createReadStream(filePath).pipe(unzip.Extract({ path: `${rootPath}` }));
                     stream.on("close", () => {
                         try { res(filePath); } catch (err) { rej(err); }
@@ -102,12 +105,12 @@ export async function getDebugger(config: vscode.DebugConfiguration) {
             // Stop debugger if running
             if (os.platform() === 'win32') {
                 // Windows stop debugger if already running
-                child_process.exec("tskill java");
+                child_process.execSync("tskill java");
             }
             else {
                 // Linux/Mac stop debugger if already running and make sure script is executable
-                child_process.exec("kill -9 $(ps -ef | grep 'daffodil' | grep 'jar' | awk '{ print $2 }') || return 0") // ensure debugger server not running and
-                child_process.exec(`chmod +x ${rootPath}/daffodil-debugger-${dapodilVersion.substring(1)}/bin/da-podil`)     // make sure da-podil is executable
+                child_process.exec("kill -9 $(ps -ef | grep 'daffodil' | grep 'jar' | awk '{ print $2 }') || return 0")     // ensure debugger server not running and
+                child_process.execSync(`chmod +x ${rootPath.replace(" ", "\\ ")}/daffodil-debugger-${dapodilVersion.substring(1)}/bin/da-podil`)    // make sure da-podil is executable
             }
 
             // Assign script name based on os platform
