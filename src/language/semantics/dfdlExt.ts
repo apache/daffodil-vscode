78016117f6b29f924998e99c655260b59a742fe4
NolanMatt <118382124+NolanMatt@users.noreply.github.com>
2023-05-31 15:27:55 -0400

    Problem tokens, possibly fully working now

--- src/language/semantics/dfdlExt.ts
+++ src/language/semantics/dfdlExt.ts
@@ -57,6 +57,7 @@ export class XPathSemanticTokensProvider
 {
   private xpLexer = new XPathLexer()
   private collection: vscode.DiagnosticCollection
+  private diagnosticList = new Array<vscode.Diagnostic>()
   public constructor(collection: vscode.DiagnosticCollection) {
     this.collection = collection
   }
@@ -124,14 +125,15 @@ export class XPathSemanticTokensProvider
       endList.push(end)
       tokens = tokens.concat(tmpTokens)
 
+      // This was moved to inside the loop. If it isn't, the sections of XPath will be treated
+      //   as a single XPath section instead of multiples
+      setTimeout(() => this.reportProblems(tmpTokens, document, variables), 0)
+
       // Reset the xpLexer. If this is not done, existing tokens will not be flushed
       //   and will be re-added to the tokens list. This might not affect the operation, but it does
       //   increase the memory required by the tokenizer, potentially running out of memory.
       this.xpLexer.reset()
     }
-    // This was moved to inside the loop. If it isn't, the sections of XPath will be treated
-    //   as a single XPath section instead of multiples
-    setTimeout(() => this.reportProblems(tokens, document, variables), 0)
 
     const builder = new vscode.SemanticTokensBuilder()
     tokens.forEach((token) => {
@@ -143,6 +145,7 @@ export class XPathSemanticTokensProvider
         0
       )
     })
+    this.diagnosticList = new Array<vscode.Diagnostic>()
     return builder.build()
   }
 
@@ -242,8 +245,11 @@ export class XPathSemanticTokensProvider
       [],
       variables
     )
-    if (diagnostics.length > 0) {
-      this.collection.set(document.uri, diagnostics)
+    diagnostics.forEach((diag) => {
+      this.diagnosticList.push(diag)
+    })
+    if (this.diagnosticList.length > 0) {
+      this.collection.set(document.uri, this.diagnosticList)
     } else {
       this.collection.clear()
     }
