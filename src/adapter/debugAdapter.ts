7dc72cecfd162d3cc21f96d486902c6304fee4d4
Shane Dell <shanedell100@gmail.com>
2023-06-15 13:52:46 -0400

    Throw parse errors
    - Update debugger to check if the parse errored. If it does it will throw the error.
    - Add some notes to the typescript for code that possibly be removed and moved.
    
    Closes #648

--- src/adapter/debugAdapter.ts
+++ src/adapter/debugAdapter.ts
@@ -1,3 +1,5 @@
+// NOTE: Might be able to removed. Only used for a test
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
