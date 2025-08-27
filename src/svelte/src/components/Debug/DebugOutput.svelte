53bffdaaafbf2e657da2a6086e058893823f1dce
Robert Strickland <rstrickland@apache.org>
2025-08-27 16:29:50 -0500

    Data Editor Svelte 5 Upgrade w/ Vite Replacement

---
+++ src/svelte/src/components/Debug/DebugOutput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { type DebugVariable } from "./Debug.svelte.ts"
+
+  let {
+    id,
+    valueStr,
+    renderVar,
+    removeVar
+  }= $props()
+
+</script>
+
+<span class="dbg-row">
+  <button onclick={removeVar} class="material-symbols-outlined">close</button>
+  {@render renderVar({id, valueStr})}
+</span>
+
+<style lang="scss">
+  span.dbg-row{
+    display: flex;
+
+  }
+</style>
